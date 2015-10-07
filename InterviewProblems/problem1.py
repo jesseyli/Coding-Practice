2dfd627bd284862a649de097b79b3cb25a61e682
Jesse <jesseyli@berkeley.edu>
2015-10-06 20:14:29 -0700

    Finished Problem 1

--- InterviewProblems/problem1.py
+++ InterviewProblems/problem1.py
@@ -1,16 +1,12 @@
 def solution(A):
-	# Return the maximum product of any slice in A. Iterates through A finding the smallest negative
-	# product and largest positive product at each array index.
-	max_pos, min_neg, output = 1.0
-	if not A:
-		return 0
-	for a in A:
-		if output > 1e9:
+	# Return the maximum product of any slice in A. 
+	maxCurr, minCurr, output = A[0],A[0],A[0]
+	for a in A[1:]:
+		if output >= 1e9:
 			return 1e9
-		if a > 0:
+		prevMax = maxCurr
+		maxCurr = max(maxCurr*a,a,minCurr*a)
+		minCurr = min(prevMax*a,a,minCurr*a)
+		output = max(output,maxCurr)
+	return output
 
-		elif a < 0:
-		elif a == 0:
-			
-
-		
